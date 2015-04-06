23e74a90912cf2f2a2614538b339aa655112e079
nicolasgastiazoro <gastiazoronicolas@gmail.com>
2015-04-06 09:55:54 -0300

    Hello World

---
+++ hello.c
@@ -0,0 +1,20 @@
+/* UTN FRBA SSL - HelloWorld
+ * hello.c
+ *
+ * 2015 04 05
+ *
+ *
+ * por Nicolas Gastiazoro
+ *
+ * Programa simple para probar el compilador.
+ * Imprime un saludo en stdout.
+ */
+
+#include <stdio.h>  /* puts */
+#include <stdlib.h> /* EXIT_SUCCESS */
+
+int main(void){
+	puts("Hello, World!");
+
+	return EXIT_SUCCESS;
+}
